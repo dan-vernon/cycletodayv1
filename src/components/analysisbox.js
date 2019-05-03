46a8494964cebf3c8cc2729c62e393bc470e0aa0
Dan <danvernon@gmail.com>
2019-05-03 16:08:07 +0100

    init basic structural components

---
+++ src/components/analysisbox.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import styles from './AnalysisBox.module.css';
+
+export default(props) => {
+  return(
+    <div className="analysis-box" />
+  )
+}
---
+++ src/components/forecastbox.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import styles from './ForecastBox.module.css';
+
+export default(props) => {
+  return(
+    <div className="forecast-box" />
+  )
+}
