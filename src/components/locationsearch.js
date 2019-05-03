bf523edaa6c4b317bf145e624e3526b72dc13b34
Dan <danvernon@gmail.com>
2019-05-03 17:37:28 +0100

    algolia init config

--- src/components/locationsearch.js
+++ src/components/locationsearch.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
 import styles from './LocationSearch.module.css';
 
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox } from 'react-instantsearch-dom';
+
 export default(props) => {
+  const searchClient = algoliasearch(
+  'QLMLIFLF87',
+  '61e837f421da50253990812e9716a46b'
+  )
+
   return(
-    <input type="text" />
+    <InstantSearch
+  indexName="instant_search"
+  searchClient={searchClient}
+  >
+    <SearchBox />
+    </InstantSearch>
+
   )
 }
