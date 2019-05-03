e4e85b292c261592ce9dc9e3444c60d05a60c19c
Dan <danvernon@gmail.com>
2019-05-03 16:09:23 +0100

    import components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+import LocationSearch from "../components/locationsearch"
+import ForecastBox from "../components/forecastbox"
+import AnalysisBox from "../components/analysisbox"
 
 const IndexPage = () => (
   <Layout>
