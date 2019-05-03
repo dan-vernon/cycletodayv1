3eeea0f82e152a96a6e23f0c74b3641bc9ebde25
Dan <danvernon@gmail.com>
2019-05-03 16:06:24 +0100

    init site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `cycletoday.cc`,
+    subTitle: `Should I cycle today?`,
+    description: `Answering the age-old daily question: should I cycle today?`,
+    author: `danvernon.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
