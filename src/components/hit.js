c371481297a1809ac59cb2d75dab96ff40546f06
Dan <danvernon@gmail.com>
2019-05-03 22:45:50 +0100

    Hit component calls MO onClick

--- src/components/hit.js
+++ src/components/hit.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const handleClick = (event) => {
-  console.log(event.target.value)
-}
+const Hit = ({ hit }) => {
+  const handleClick = () => {
+    console.log(hit.id)
+    const forecastBox = document.getElementById(hit.id)
+    fetch(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${hit.id}?res=3hourly&key=${process.env.GATSBY_MO_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        const today = data.SiteRep.DV.Location.Period[0].Rep
+        forecastBox.innerHTML = ""
+        today.map((rep) => {
+          return forecastBox.innerHTML += `<tr>
+          <td>${rep['$']/60}00h</td><td>feels like ${rep['F']}</td>
+          </tr>
+          `
+        })
+      });
+  }
 
 const Hit = ({ hit }) => (
   <div className="hit">
