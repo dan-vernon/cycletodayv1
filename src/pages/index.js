4c244825d20452f4f8f4ab9429eb948cdea0199d
Dan <danvernon@gmail.com>
2019-05-03 16:09:57 +0100

    component structure within layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,10 @@ import AnalysisBox from "../components/analysisbox"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Home" keywords={[`cycle`, `cycle commute`, `weather`]} />
+    <LocationSearch />
+    <ForecastBox />
+    <AnalysisBox />
   </Layout>
 )
 
