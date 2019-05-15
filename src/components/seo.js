c43c9167e78bb7b46744a7dca702b7f78606b4cd
Dan <danvernon@gmail.com>
2019-05-15 20:18:16 +0100

    import FA and google font

--- src/components/seo.js
+++ src/components/seo.js
@@ -77,7 +77,10 @@ function SEO({ description, lang, meta, keywords, title }) {
             : []
         )
         .concat(meta)}
-    />
+    >
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet" />
+    </ Helmet>
   )
 }
 
